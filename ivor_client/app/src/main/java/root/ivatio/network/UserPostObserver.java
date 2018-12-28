45699990cca00278076f616a6103bf15b9dc2cd5
Игорь Смирнов <cool.rainbow2012@yandex.ru>
2018-12-28 18:03:26 +0300

    Изменена структура проекта

--- ivor_client/app/src/main/java/root/ivatio/network/UserPostObserver.java
+++ ivor_client/app/src/main/java/root/ivatio/network/UserPostObserver.java
@@ -40,6 +40,6 @@ public class UserPostObserver implements SingleObserver<User> {
     }
 
     public void unsubscribe() {
-        disposable.dispose();
+        if (disposable != null) disposable.dispose();
     }
 }
